944826054de96e8057f7762896e31f634177aa10
GGraca <ggracase@gmail.com>
2016-04-11 00:12:46 +0100

    Added Bash and Xorg

--- install.sh
+++ install.sh
@@ -1,15 +1,29 @@
+#Openbox
 rm ~/.config/openbox
 rm -r ~/.config/openbox
 ln -s $(echo $(pwd)/openbox) ~/.config/openbox
 
+#Tint2
 rm ~/.config/tint2
 rm -r ~/.config/tint2
 ln -s $(echo $(pwd)/tint2) ~/.config/tint2
 
+#Conky
 rm ~/.config/conky
 rm -r ~/.config/conky
 ln -s $(echo $(pwd)/conky) ~/.config/conky
 
+#I3
 rm ~/.config/i3
 rm -r ~/.config/i3
 ln -s $(echo $(pwd)/i3) ~/.config/i3
+
+#Xorg
+rm ~/.Xmodmap
+ln -s $(echo $(pwd)/xorg/.Xmodmap) ~/.Xmodmap
+rm ~/.xinitrc
+ln -s $(echo $(pwd)/xorg/.xinitrc) ~/.xinitrc
+
+#Bash
+rm ~/.bashrc
+ln -s $(echo $(pwd)/bash/.bashrc) ~/.bashrc
