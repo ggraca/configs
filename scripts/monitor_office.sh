d54619792be5a54654ca02b31d16319ac6774fd3
GGraca <ggracase@gmail.com>
2019-05-27 19:30:07 +0100

    Added compton and dunst. Improved Polybar

--- scripts/monitor_office.sh
+++ scripts/monitor_office.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Top
-xrandr --output DP1 --mode 1920x1080 --above eDP1
+xrandr --output DP1 --mode 1920x1080 --above eDP-1
 
 # Left
-#xrandr --output eDP1 --pos 1680x0 --output DP1 --mode 1680x1050_60.00 --pos 0x30
+#xrandr --output eDP-1 --pos 1680x0 --output DP1 --mode 1680x1050_60.00 --pos 0x30
 
 # Right
-#xrandr --output eDP1 --pos 0x0 --output DP1 --mode 1680x1050_60.00 --pos 1920x30
+#xrandr --output eDP-1 --pos 0x0 --output DP1 --mode 1680x1050_60.00 --pos 1920x30
 
 # Right Rotated
-#xrandr --output eDP1 --pos 0x600 --output DP1 --mode 1680x1050_60.00 --rotate left --pos 1920x0
+#xrandr --output eDP-1 --pos 0x600 --output DP1 --mode 1680x1050_60.00 --rotate left --pos 1920x0
 
 feh --bg-scale ~/Pictures/Wallpapers/Primary.jpg --bg-scale ~/Pictures/Wallpapers/Secondary.jpg
--- scripts/monitor_office_old.sh
+++ scripts/monitor_office_old.sh
@@ -5,15 +5,15 @@ xrandr --newmode "1680x1050_60.00"  146.25  1680 1784 1960 2240  1050 1053 1059 
 xrandr --addmode DP1 "1680x1050_60.00"
 
 # Top
-xrandr --output eDP1 --pos 0x1050 --output DP1 --mode 1680x1050_60.00 --pos 120x0
+xrandr --output eDP-1 --pos 0x1050 --output DP1 --mode 1680x1050_60.00 --pos 120x0
 
 # Left
-#xrandr --output eDP1 --pos 1680x0 --output DP1 --mode 1680x1050_60.00 --pos 0x30
+#xrandr --output eDP-1 --pos 1680x0 --output DP1 --mode 1680x1050_60.00 --pos 0x30
 
 # Right
-#xrandr --output eDP1 --pos 0x0 --output DP1 --mode 1680x1050_60.00 --pos 1920x30
+#xrandr --output eDP-1 --pos 0x0 --output DP1 --mode 1680x1050_60.00 --pos 1920x30
 
 # Right Rotated
-#xrandr --output eDP1 --pos 0x600 --output DP1 --mode 1680x1050_60.00 --rotate left --pos 1920x0
+#xrandr --output eDP-1 --pos 0x600 --output DP1 --mode 1680x1050_60.00 --rotate left --pos 1920x0
 
 feh --bg-scale ~/Pictures/Wallpapers/Primary.jpg --bg-scale ~/Pictures/Wallpapers/Secondary.jpg
